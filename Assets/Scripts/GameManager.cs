fa669a2c91fe5901e182e7d534b9bca5acba3981
mugwhump <grayson.bartlet@shaw.ca>
2025-01-26 13:25:47 -0800

    Added title screen and victory screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("TileMapScene");
+    }
+    
+    public void WinGame() {
+        SceneManager.LoadScene("Victory_Screen");
+    }
+}
